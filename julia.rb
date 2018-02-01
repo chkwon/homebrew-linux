a85c43f6214559dc2395f541f4a1dbce742bd1e0
Changhyun Kwon <chkwon@gmail.com>
2018-02-01 16:59:53 -0500

    juila update

--- julia.rb
+++ julia.rb
@@ -2,7 +2,7 @@ class Julia < Formula
     homepage "http://julialang.org"
     version "0.6.2"
 
-    url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-linux-x86_64.tar.gz"
+    url "https://julialang-s3.julialang.org/bin/mac/x64/0.6/julia-#{version}-linux-x86_64.tar.gz"
     sha256 "dc6ec0b13551ce78083a5849268b20684421d46a7ec46b17ec1fab88a5078580"
 
     bottle :unneeded
